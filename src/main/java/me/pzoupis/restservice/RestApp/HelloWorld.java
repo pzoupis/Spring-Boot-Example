82b6e2f272deff8919a9d117fe8678d270557630
Pantelis Zoupis <pantelis.zoupis@gmail.com>
2018-07-15 21:58:59 +0300

    Add car endpoint with path variable

--- src/main/java/me/pzoupis/restservice/RestApp/HelloWorld.java
+++ src/main/java/me/pzoupis/restservice/RestApp/HelloWorld.java
@@ -1,7 +1,11 @@
 package me.pzoupis.restservice.RestApp;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class HelloWorld {
@@ -9,5 +13,12 @@ public class HelloWorld {
     @RequestMapping("/")
     public String index() {
         return "Hello World";
+    }
+
+    @RequestMapping(value = "/car/{id}",
+            method = GET)
+    @ResponseBody
+    public String test(@PathVariable("id") long id) {
+        return "Car with id: " + id;
     }
 }
