5a7cd7fc3c0ca0e8875710fb9ab9458cbeb68bf9
Pantelis Zoupis <pantelis.zoupis@gmail.com>
2018-07-15 20:47:14 +0300

    Initial commit

---
+++ src/main/java/me/pzoupis/restservice/RestApp/HelloWorld.java
@@ -0,0 +1,13 @@
+package me.pzoupis.restservice.RestApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
